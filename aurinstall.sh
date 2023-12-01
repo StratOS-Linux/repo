624532bf7ef50eab15308f3253df0b3faadaf53b
slipstream8125 <hatkardhruv@gmail.com>
2023-12-02 00:37:11 +0530

    Updated repo

---
+++ aurinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Enter the package you want to add to the repo : "
+read packagename
+aur sync --repo LUGOS-repo --root x86_64 $packagename
+repo-add -n x86_64/LUGOS-repo.db.tar.gz x86_64/*.pkg.tar.zst
+rm x86_64/LUGOS-repo.db x86_64/LUGOS-repo.files
+mv x86_64/LUGOS-repo.db.tar.gz x86_64/LUGOS-repo.db
+mv x86_64/LUGOS-repo.files.tar.gz x86_64/LUGOS-repo.files
+git add .
+git commit -m "Updated repo"
+git push
---
+++ initialize.sh
@@ -0,0 +1,2 @@
+repo-add -n x86_64/LUGOS-repo.db.tar.gz x86_64/*.pkg.tar.zst
+
