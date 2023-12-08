5e42d0f7787f4dd0650c8ea0298161db59e42cc2
slipstream8125 <hatkardhruv@gmail.com>
2023-12-08 22:55:08 +0530

    Added lugfetch package

--- installpackages.sh
+++ installpackages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sh initialize.sh
 echo "Enter the package you want to add to the repo : "
 read packagename
 aur sync --repo LUGOS-repo --root x86_64 $packagename
@@ -7,5 +8,5 @@ rm x86_64/LUGOS-repo.db x86_64/LUGOS-repo.files
 mv x86_64/LUGOS-repo.db.tar.gz x86_64/LUGOS-repo.db
 mv x86_64/LUGOS-repo.files.tar.gz x86_64/LUGOS-repo.files
 git add .
-git commit -m "Updated repo"
+git commit -m "Added $packagename to the repo"
 git push
