fd3333ea6413f3e71351fc9fc9f1c01a99be386a
Slipstream <hatkardhruv@gmail.com>
2023-12-27 12:33:26 +0530

    Updated DB

--- installpackages.sh
+++ installpackages.sh
@@ -4,9 +4,6 @@ read packagename
 aur sync --repo LUGOS-repo --root x86_64 $packagename
 echo "Updating Database....."
 repo-add -n x86_64/LUGOS-repo.db.tar.gz x86_64/*.pkg.tar.zst
-rm x86_64/LUGOS-repo.db x86_64/LUGOS-repo.files
-mv x86_64/LUGOS-repo.db.tar.gz x86_64/LUGOS-repo.db
-mv x86_64/LUGOS-repo.files.tar.gz x86_64/LUGOS-repo.files
 echo "Pushing files to repo...."
 git add .
 git commit -m "Added $packagename to the repo"
