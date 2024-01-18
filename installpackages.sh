19c6fd982e6a39adca32cfcac2c663db85b23b82
Slipstream <hatkardhruv@gmail.com>
2024-01-18 09:51:27 +0530

    Added libadwaita-without-adwaita-git

--- installpackages.sh
+++ installpackages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Enter the package you want to add to the repo : "
 read packagename
-aur sync --repo LUGOS-repo --root x86_64 $packagename
+aur sync --repo StratOS-repo --root x86_64 $packagename
 echo "Updating Database....."
-repo-add -n x86_64/LUGOS-repo.db.tar.gz x86_64/*.pkg.tar.zst
+repo-add -n x86_64/StratOS-repo.db.tar.gz x86_64/*.pkg.tar.zst
 echo "Pushing files to repo...."
 git add .
 git commit -m "Added $packagename to the repo"
