33e10ea7f7e7c26dc8df5ca2835c43c2dc6ae81a
ZeStig <69384921+zstg@users.noreply.github.com>
2024-08-10 19:25:48 +0530

    Remove all traces of the old StratOS repo before re-initializing it
    - All built packages are retained
    - db zst file is recreated

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # repo-add -n x86_64/StratOS-repo.db.tar.gz x86_64/*.pkg.tar.zst**
 # Check cassava/repoctl README for tips
+rm -rf x86_64/StratOS-repo**
 repoctl conf new x86_64/StratOS-repo.db.tar.zst
 repoctl reset
